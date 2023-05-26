024c7be01a4e79d8aa52ade4cfbaa09ffa330fec
Valen-arche <110450286+Valen-arche@users.noreply.github.com>
2023-05-25 22:32:39 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const nombre= 'Valentin';
+const edad= 23;
+const altura= 1.73;
+const casado= false;
+document.write("Mi nombre es " + nombre + ". Tengo " + edad + " años, tengo una estatura de " + altura);
+console.log(nombre);
+console.log("Mi nombre es " + nombre + ". Tengo " + edad + " años, tengo una estatura de " + altura) ;
