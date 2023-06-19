d0a28a3dc74b3bc47ab79dd1b1753f4cc5c897d4
Valen-arche <110450286+Valen-arche@users.noreply.github.com>
2023-06-19 00:43:17 -0300

    Update and rename javascript.js to calculadora.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-const nombre= 'Valentin';
-const edad= 23;
-const altura= 1.73;
-const casado= false;
-document.write("Mi nombre es " + nombre + ". Tengo " + edad + " años, tengo una estatura de " + altura);
-console.log(nombre);
-console.log("Mi nombre es " + nombre + ". Tengo " + edad + " años, tengo una estatura de " + altura) ;
